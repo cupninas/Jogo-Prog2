fbfd19941ea173b73cb101d858e9da583bbe811a
aimee <aimeemiranribe@gmail.com>
2025-02-16 19:20:01 -0300

    adicionando classe abstrata

--- Classe.java
+++ Classe.java
@@ -1,10 +1,10 @@
 package Jogo;
 
-public class Classe {
-	private String nome;
-	private String arma;
-	private String tipoDano;
-	private String habilidade;
+public abstract class Classe {
+	protected String nome;
+	protected String arma;
+	protected String tipoDano;
+	protected String habilidade;
 	
 	public Classe(String nome, String arma, String tipoDano, String habilidade) {
 		this.nome = nome;
@@ -12,37 +12,4 @@ public class Classe {
 		this.tipoDano = tipoDano;
 		this.habilidade = habilidade;
 	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getArma() {
-		return arma;
-	}
-
-	public void setArma(String arma) {
-		this.arma = arma;
-	}
-
-	public String getTipoDano() {
-		return tipoDano;
-	}
-
-	public void setTipoDano(String tipoDano) {
-		this.tipoDano = tipoDano;
-	}
-
-	public String getHabilidade() {
-		return habilidade;
-	}
-
-	public void setHabilidade(String habilidade) {
-		this.habilidade = habilidade;
-	}
-	
 }
