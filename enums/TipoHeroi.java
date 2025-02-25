0f91a33389442f05dc03dae318575c93c695a8cc
ivangrana <ivangrana1956@gmail.com>
2025-02-25 10:45:55 -0300

    .

--- enums/TipoHeroi.java
+++ enums/TipoHeroi.java
@@ -2,21 +2,23 @@ package Jogo.enums;
 
 
 public enum TipoHeroi {
-    GUERREIRO(0, "Guerreiro", "Especialista em combate corpo a corpo, usa força e resistência para vencer seus inimigos.", 5),
-    MAGO(1, "Mago", "Mestre das artes arcanas, canaliza energia mágica para lançar feitiços poderosos.", 4),
-    ARQUEIRO(2, "Arqueiro", "Exímio atirador, utiliza arcos e flechas para atacar à distância com precisão mortal.", 3),
-    LADINO(3, "Ladino", "Ágil e furtivo, prefere ataques rápidos e estratégicos, muitas vezes agindo nas sombras.", 5);
+    GUERREIRO(0, "Guerreiro", "Especialista em combate corpo a corpo, usa força e resistência para vencer seus inimigos.", 5, 20),
+    MAGO(1, "Mago", "Mestre das artes arcanas, canaliza energia mágica para lançar feitiços poderosos.", 4, 30),
+    ARQUEIRO(2, "Arqueiro", "Exímio atirador, utiliza arcos e flechas para atacar à distância com precisão mortal.", 3, 25),
+    LADINO(3, "Ladino", "Ágil e furtivo, prefere ataques rápidos e estratégicos, muitas vezes agindo nas sombras.", 5, 20);
 
     private final int id;
     private final String nome;
     private final String descricao;
     private final int ataque;
+    private final int defesa;
 
-    TipoHeroi(int id, String nome, String descricao, int ataque) {
+    TipoHeroi(int id, String nome, String descricao, int ataque, int defesa) {
         this.id = id;
         this.nome = nome;
         this.descricao = descricao;
         this.ataque = ataque;
+        this.defesa = defesa;
     }
 
     public int getId() {
@@ -33,5 +35,9 @@ public enum TipoHeroi {
 
     public int getAtaque() {
         return ataque;
+    }
+
+    public int getDefesa() {
+        return defesa;
     }
 }
